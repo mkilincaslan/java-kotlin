6ba8806622898f75953e58cca727df62d745a9ea
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2020-10-18 21:59:52 +0300

    insta clone app with java begin

---
+++ InstaCloneFirebase/app/src/main/java/com/example/instaclonefirebase/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.instaclonefirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void signInAction (View view) {
+
+    }
+
+    public void signUpAction (View view) {
+    }
+}
