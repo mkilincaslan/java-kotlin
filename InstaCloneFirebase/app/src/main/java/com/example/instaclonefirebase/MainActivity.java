75245270f2e164da509e6163f1c8d5254b80dd86
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2020-10-18 22:05:37 +0300

    input define & create firebase initialize

--- InstaCloneFirebase/app/src/main/java/com/example/instaclonefirebase/MainActivity.java
+++ InstaCloneFirebase/app/src/main/java/com/example/instaclonefirebase/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.instaclonefirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth firebaseAuth;
+    EditText txtEmail, txtPassword;
 
 
     @Override
@@ -17,7 +24,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // firebase instance oluşturma
+        firebaseAuth = FirebaseAuth.getInstance();
 
+        // Inputlarımızı tanımlama
+        txtEmail = findViewById(R.id.txtEmail);
+        txtPassword = findViewById(R.id.txtPassword);
     }
 
     public void signInAction (View view) {
@@ -25,5 +37,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void signUpAction (View view) {
+        String email = txtEmail.getText().toString();
+        String password = txtPassword.getText().toString();
+
+        if (email.matches("") || password.matches("")) {
+            Toast.makeText(this, "email or password cannot be empty", Toast.LENGTH_SHORT).show();
+        } else {
+
+            // firebase email ve password kullanarak kullanıcı oluşturma
+            firebaseAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                        }
+                    });
+        }
     }
 }
