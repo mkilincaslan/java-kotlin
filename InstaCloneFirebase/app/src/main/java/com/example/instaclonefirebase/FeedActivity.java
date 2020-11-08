a72822f8048e9c2feec6c9451bd1d90aa91a99ff
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2020-11-09 00:55:57 +0300

    get data from firestore on feed activity

--- InstaCloneFirebase/app/src/main/java/com/example/instaclonefirebase/FeedActivity.java
+++ InstaCloneFirebase/app/src/main/java/com/example/instaclonefirebase/FeedActivity.java
@@ -1,6 +1,7 @@
 package com.example.instaclonefirebase;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,12 +9,22 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.List;
+import java.util.Map;
 
 public class FeedActivity extends AppCompatActivity {
 
-    FirebaseAuth firebaseAuth;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firebaseFirestore;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -50,5 +61,30 @@ public class FeedActivity extends AppCompatActivity {
         setContentView(R.layout.activity_feed);
 
         firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        getData();
+    }
+
+    public void getData() {
+        firebaseFirestore.collection("Posts").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                if (error != null) {
+                    Toast.makeText(FeedActivity.this, error.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                } else {
+                    if (value != null) {
+                        for (DocumentSnapshot snapshot : value.getDocuments()) {
+                            Map<String, Object> data = snapshot.getData();
+                            String comment = (String) data.get("comment");
+                            String userEmail = (String) data.get("userEmail");
+                            String downloadURL = (String) data.get("downloadURL");
+
+                            System.out.println("comment" + comment);
+                        }
+                    }
+                }
+            }
+        });
     }
 }
