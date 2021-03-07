0ddebb3a3704cf3186673d5c58745cd3571d1126
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2021-03-07 21:52:19 +0300

    parse app initalize

---
+++ ParseApp/app/src/main/java/com/example/parseapp/ParseStarter.java
@@ -0,0 +1,23 @@
+package com.example.parseapp;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class ParseStarter extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG); // Define logcat setting
+
+        Parse.initialize(
+                new Parse.Configuration.Builder(this)
+                        .applicationId(getString(R.string.applicationId))
+                        .clientKey(getString(R.string.clientKey))
+                        .server(getString(R.string.server))
+                        .build()
+        );
+    }
+}
