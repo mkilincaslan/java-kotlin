deab4f323da3cdfe3479a24a876de4c8443176bf
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2021-03-08 01:44:58 +0300

    get data from parse server on parse app

--- ParseApp/app/src/main/java/com/example/parseapp/MainActivity.java
+++ ParseApp/app/src/main/java/com/example/parseapp/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,7 +18,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Create an Parse Object instance to save data to Parse Object
+//        addData();
+//        getData();
+    }
+
+    public void getData () {
+        // Create a Parse Query instance to get data from Parse server
+        // Verileri getirmek için Parse Query nesnesi oluşturma
+        ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("Fruits");
+
+        // Get data which is given Id to the query
+        // Id verilen nesnenin arka planda getirilmesini sağlama
+        parseQuery.getInBackground("1CBy9ul7Wc", new GetCallback<ParseObject>() {
+            @Override
+            public void done(ParseObject object, ParseException e) {
+                if (e != null) {
+                    e.printStackTrace();
+                } else {
+                    String name = object.getString("name");
+                    int calory = object.getInt("calories");
+
+                    System.out.println("object name: " + name);
+                    System.out.println("object calory: " + calory);
+                }
+            }
+        });
+    }
+
+    public void addData () {
+        // Create a Parse Object instance to save data to Parse server
         // Verileri kaydetmek için Parse nesnesi örneği oluşturma
         ParseObject parseObject = new ParseObject("Fruits");
 
