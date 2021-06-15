4ebc29312b651df530d403fc6fd7891c22e6da0c
Mesut <kilincaslanmesut@gmail.com>
2021-06-15 09:25:59 +0300

    InstagramParseClone
    Permissions & Put image in imageview

--- InstagramParseClone/app/src/main/java/com/example/instagramparseclone/PostActivity.java
+++ InstagramParseClone/app/src/main/java/com/example/instagramparseclone/PostActivity.java
@@ -1,11 +1,25 @@
 package com.example.instagramparseclone;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class PostActivity extends AppCompatActivity {
 
@@ -23,5 +37,48 @@ public class PostActivity extends AppCompatActivity {
 
     public void post(View view) {
 
+    }
+
+    public void chooseImage(View view) {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        } else {
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent, 2);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (requestCode == 1 ) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 2);
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if (requestCode == 2 && resultCode == RESULT_OK && data != null) {
+            Uri uri = data.getData();
+            try {
+                if (Build.VERSION.SDK_INT >= 28) {
+                    ImageDecoder.Source source = ImageDecoder.createSource(this.getContentResolver(), uri);
+                    Bitmap bitmap = ImageDecoder.decodeBitmap(source);
+                    postImageView.setImageBitmap(bitmap);
+                } else {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+                    postImageView.setImageBitmap(bitmap);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
